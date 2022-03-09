58a196c90109a033428df365ac661b51d517d3fd
Lukas Barak <lukas.barak@unicorn.com>
2022-03-09 23:29:40 +0100

    Exercise 4 green

--- src/lib/factory.js
+++ src/lib/factory.js
@@ -1,10 +1,67 @@
 const warehouse = require("./warehouse");
 
-function CarFactory(power = 10) {
+class Factory {
+    
+    constructor(power = 10){
+        this.power = power;
+        this.warehouse = new warehouse();
+    }
 
 
+    produceCar(color="red", wheels=4,engine=false)
+    {        
+        if(this.power < 2){
+            // todo - boost
+            console.log("No power no fun.");
+            return null;
+        }
+        
+        this.power -= 2;
+        
+        
+        let newCar = {
+            id: this.warehouse.getNextId(), //auto incremented value which should be stored in warehouse
+            color: color, //from param
+            wheels: wheels, //from param
+            engine: engine, //from param
+        }
+        
+      
+        this.warehouse.parkCar(newCar); // park car
+
+        return newCar; 
+    }
+
+    changeCarColor(car, newColor)
+    {
+        
+      /*    
+        if(!newColor)
+        {
+            // no color defined
+            return null;
+        } 
+        */
+
+
+        if(this.power < 1){
+            // todo - boost
+            return null;
+
+        }
+        
+        this.power--;
+        
+
+        car.color = car.color === 'red' ? 'blue' : 'red';
+        return car;
+    }
+
+    addEnergyPower(power){
+        this.power += power;
+    }
 }
 
-module.exports = CarFactory;
+module.exports = Factory;
 
 
--- src/lib/warehouse.js
+++ src/lib/warehouse.js
@@ -1,6 +1,22 @@
-let warehouse = {
-  createdCars: [],
-  nextIdentifier: 0
+
+
+class warehouse  { 
+
+  constructor(){
+
+    this.createdCars=[];
+    this.nextIdentifier = 0; 
+  }  
+
+  getNextId(){
+    return this.nextIdentifier++;
+  }
+
+  parkCar(car){
+    this.createdCars.push(car);
+  }
+
+   //getNextId: function () { return this.nextIdentifier++; }
 };
 
 module.exports = warehouse;
